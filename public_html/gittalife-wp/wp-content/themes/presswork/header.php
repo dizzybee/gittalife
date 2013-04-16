335c7ec397624aabe5013fcefc249d40bbf708fd
Eric Bastholm <eric.bastholm@systemic.com.au>
2013-04-16 23:40:52 +0800

    Fresh download after installing more plugins.
    Plugins installed:
    Redirection
    Google Analytics Dashboard for WP
    Google Analytics for WordPress (inactive)
    WordPress SEO

--- public_html/gittalife-wp/wp-content/themes/presswork/header.php
+++ public_html/gittalife-wp/wp-content/themes/presswork/header.php
@@ -26,6 +26,16 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width" />
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40167778-1', 'massagetherapyperth.com.au');
+  ga('send', 'pageview');
+
+</script>
 </head>
 
 <!--[if lt IE 7 ]> <body <?php body_class("ie6"); ?>> <![endif]-->
